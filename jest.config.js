b0acecf7b78ee69bfa0d4380b07db0711bf9bb7b
angelr1076 <angelr1076@gmail.com>
2023-04-13 07:35:57 -0400

    Added styles folder, .babelrc and jest.config.js files along with dependencies for testing and test modules

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  modulePaths: ['/shared/vendor/modules'],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  moduleDirectories: ['node_modules', 'shared'],
+
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': 'identity-obj-proxy',
+  },
+};
