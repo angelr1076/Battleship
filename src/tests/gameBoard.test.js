d5532ea4d061326f5a4268c111d35cdb9a83f7a9
angelr1076 <angelr1076@gmail.com>
2023-04-15 14:50:52 -0400

    Added all tests for createShip and createGameBoard functions

--- src/tests/gameBoard.test.js
+++ src/tests/gameBoard.test.js
@@ -43,4 +43,29 @@ describe('Gameboard factory function tests', () => {
       gameboard.placeShip(3, 8, 2, 'diagonal');
     }).toThrow("Invalid orientation. Must be 'horizontal' or 'vertical'.");
   });
+
+  it('should be able to report whether or not all of their ships have been sunk', () => {
+    const gameboard = createGameBoard();
+    gameboard.placeShip(3, 8, 2, 'horizontal');
+    gameboard.placeShip(4, 5, 6, 'vertical');
+    gameboard.receiveAttack(8, 2);
+    gameboard.receiveAttack(8, 3);
+    gameboard.receiveAttack(8, 4);
+    gameboard.receiveAttack(5, 6);
+    gameboard.receiveAttack(6, 6);
+    gameboard.receiveAttack(7, 6);
+    gameboard.receiveAttack(8, 6);
+    expect(gameboard.allShipsSunk()).toBe(true);
+  });
+
+  it('should keep track of missed attacks', () => {
+    const gameboard = createGameBoard();
+    gameboard.placeShip(3, 8, 2, 'horizontal');
+    gameboard.receiveAttack(2, 3);
+    gameboard.receiveAttack(5, 1);
+    expect(gameboard.missedAttacks).toEqual([
+      { x: 2, y: 3 },
+      { x: 5, y: 1 },
+    ]);
+  });
 });
