b84a450bc947a126f1133c568f23a0b299aff0d6
angelr1076 <angelr1076@gmail.com>
2023-04-26 07:14:52 -0400

    Added smartAI to computer attacks by adding adjacent coordinate detection on hits and misses

--- src/components/player.js
+++ src/components/player.js
@@ -22,12 +22,44 @@ const createPlayer = (name, isComputer = false) => {
     return Math.floor(Math.random() * 10);
   };
 
+  const lastHit = { x: -1, y: -1, adjacents: [] };
+
+  const getNextAdjacentCoordinate = () => {
+    if (lastHit.adjacents.length > 0) {
+      return lastHit.adjacents.pop();
+    }
+    return null;
+  };
+
+  const updateAdjacents = (x, y) => {
+    const possibleAdjacents = [
+      { x: x - 1, y },
+      { x: x + 1, y },
+      { x, y: y - 1 },
+      { x, y: y + 1 },
+    ];
+    lastHit.adjacents = possibleAdjacents.filter(
+      coord =>
+        coord.x >= 0 &&
+        coord.x < 10 &&
+        coord.y >= 0 &&
+        coord.y < 10 &&
+        !previousAttacks.has(`${coord.x},${coord.y}`),
+    );
+  };
+
   const aiAttack = enemyGameboard => {
     let x, y;
-    do {
-      x = getRandomCoordinate();
-      y = getRandomCoordinate();
-    } while (previousAttacks.has(`${x},${y}`));
+    const nextCoord = getNextAdjacentCoordinate();
+    if (nextCoord) {
+      x = nextCoord.x;
+      y = nextCoord.y;
+    } else {
+      do {
+        x = getRandomCoordinate();
+        y = getRandomCoordinate();
+      } while (previousAttacks.has(`${x},${y}`));
+    }
     const attackResult = attack(enemyGameboard, x, y);
     const msg = document.querySelector('#message');
     if (attackResult) {
@@ -36,6 +68,13 @@ const createPlayer = (name, isComputer = false) => {
 
       if (shipSunk) {
         showMsgTimed(`Your ${attackResult.name}'s been sunk!`, msg, 1000);
+        lastHit.x = -1;
+        lastHit.y = -1;
+        lastHit.adjacents = [];
+      } else {
+        lastHit.x = x;
+        lastHit.y = y;
+        updateAdjacents(x, y);
       }
     }
     return { x, y, attackResult };
